611db56052ea5b812228d009b83f4e339595e504
marcodem11 <marcodemicheleta01@outlook.it>
2022-03-11 10:23:52 +0100

    ;

---
+++ src/components/myTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="title">
+    <h1>
+      La mia prima app con Vue CLI!
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'myTitle',
+}
+</script>
+
+
+<style scoped>
+  .title{
+    color: #41B883;
+  }
+</style>
