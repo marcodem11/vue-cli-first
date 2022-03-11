5c93b6d5cf8468f342092f796a65c893d9d0d6bf
marcodem11 <marcodemicheleta01@outlook.it>
2022-03-11 10:24:19 +0100

    app.vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <myTitle /> 
+    <myJumbo />
+  </div>
+</template>
+
+<script>
+import myTitle from './components/myTitle.vue'
+import myJumbo from './components/hero.vue'
+export default {
+  name: 'App',
+  components: {
+    myTitle,
+    myJumbo
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
